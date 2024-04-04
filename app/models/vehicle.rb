f7db1e1ae746f2f95b6abfe505053a017650c4d7
Lucas Luan de Melo <lucasluandemelo@gmail.com>
2024-04-04 19:35:46 -0300

    style: rubocop

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,7 @@
 class Vehicle < ApplicationRecord
   belongs_to :user
 
-  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1800, less_than_or_equal_to: Date.current.year }
+  validates :year,
+            numericality: { only_integer: true, greater_than_or_equal_to: 1800,
+                            less_than_or_equal_to: Date.current.year }
 end
